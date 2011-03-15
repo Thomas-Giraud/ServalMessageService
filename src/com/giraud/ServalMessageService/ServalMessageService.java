a5ae36c92f38e0acc87a557aaed863ba1a426045
Thomas.Giraud <thomas.giraud@gmail.com>
2011-03-15 17:31:01 +1030

    First Commit

---
+++ src/com/giraud/ServalMessageService/ServalMessageService.java
@@ -0,0 +1,13 @@
+package com.giraud.ServalMessageService;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ServalMessageService extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
